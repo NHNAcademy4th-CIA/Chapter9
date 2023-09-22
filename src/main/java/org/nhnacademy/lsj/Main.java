2e12fea9b1626f77d299acaf2c26520b2b4896a1
masiljangajji <hjk172262@gmail.com>
2023-09-22 15:43:11 +0900

    feat: Main 추가

---
+++ src/main/java/org/nhnacademy/lsj/Main.java
@@ -0,0 +1,16 @@
+package org.nhnacademy.lsj;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Problem1.problem1();
+        //Problem2.problem2();
+        Problem3.problem3();
+        //Problem4.problem4();
+        //Problem5.problem5();
+        //Problem6.problem6();
+
+    }
+
+}
